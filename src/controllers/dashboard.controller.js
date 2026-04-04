79333daa219ad99251e2732302bea8026d9e49d7
Atharva Ingale <aringale17@gmail.com>
2026-04-04 22:31:27 +0530

    Modified to solve the error

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,26 +2,22 @@ import Record from "../models/record.model.js";
 
 export const getDashboard = async (req, res) => {
   try {
-    // 1️⃣ Total Income
+    const role = req.user.role;
+
     const incomeResult = await Record.aggregate([
       { $match: { type: "income" } },
       { $group: { _id: null, total: { $sum: "$amount" } } },
     ]);
 
-    const totalIncome = incomeResult[0]?.total || 0;
-
-    // 2️⃣ Total Expense
     const expenseResult = await Record.aggregate([
       { $match: { type: "expense" } },
       { $group: { _id: null, total: { $sum: "$amount" } } },
     ]);
 
+    const totalIncome = incomeResult[0]?.total || 0;
     const totalExpense = expenseResult[0]?.total || 0;
-
-    // 3️⃣ Net Balance
     const netBalance = totalIncome - totalExpense;
 
-    // 4️⃣ Category-wise totals
     const categoryWise = await Record.aggregate([
       {
         $group: {
@@ -31,33 +27,24 @@ export const getDashboard = async (req, res) => {
       },
     ]);
 
-    // 5️⃣ Recent Transactions
-    const recentTransactions = await Record.find()
-      .sort({ date: -1 })
-      .limit(5);
-
-    const monthlyTrend = await Record.aggregate([
-    {
-      $group: {
-        _id: {
-          year: { $year: "$date" },
-          month: { $month: "$date" },
-          type: "$type",
-        },
-        total: { $sum: "$amount" },
-      },
-    },
-    { $sort: { "_id.year": 1, "_id.month": 1 } },
-  ]);
-
-    res.json({
+    // Base response (for all roles)
+    let response = {
       totalIncome,
       totalExpense,
       netBalance,
       categoryWise,
-      recentTransactions,
-      monthlyTrend,
-    });
+    };
+
+    // Only Analyst & Admin can see recent transactions
+    if (role === "admin" || role === "analyst") {
+      const recentTransactions = await Record.find()
+        .sort({ date: -1 })
+        .limit(5);
+
+      response.recentTransactions = recentTransactions;
+    }
+
+    res.json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
